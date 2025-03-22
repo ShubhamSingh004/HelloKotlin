16b1a5db026e88a234ae0e046ab347209414c013
Shubham <shubhamsinghmys@gmail.com>
2025-03-22 13:11:35 +0530

    First Kotlin Code

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var a: String = "hi"
+    a = "Hello"
+
+    val marks: Int = 34
+    println(a + " Shubham you have scored $marks")
+//    test("Hello I am Test")
+}
+
+fun test(str: String){
+    println(str)
+}
